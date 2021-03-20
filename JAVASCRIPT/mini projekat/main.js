241e0a23a701fcdcf97371d0a3b24546ad04d568
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-20 11:18:33 +0100

    varijable za prikaze

---
+++ JAVASCRIPT/mini projekat/main.js
@@ -0,0 +1,6 @@
+var jsView = document.querySelector('#jsView');
+var phpView = document.querySelector('#phpView');
+var wpView = document.querySelector('#wpView');
+var jsViewImg = document.querySelector('#jsViewImg');
+var phpViewImg = document.querySelector('#phpViewImg');
+var wpViewImg = document.querySelector('#wpViewImg');
