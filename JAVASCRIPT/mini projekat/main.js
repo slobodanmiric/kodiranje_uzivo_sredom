4a64b034f17eb0a760e82a08f98898440622c884
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-20 11:25:38 +0100

    prikzi uradjeni kroz JS, sa funkcijama na click

--- JAVASCRIPT/mini projekat/main.js
+++ JAVASCRIPT/mini projekat/main.js
@@ -4,3 +4,29 @@ var wpView = document.querySelector('#wpView');
 var jsViewImg = document.querySelector('#jsViewImg');
 var phpViewImg = document.querySelector('#phpViewImg');
 var wpViewImg = document.querySelector('#wpViewImg');
+
+function showJsView() {
+    jsView.style.display = "block";
+    phpView.style.display = 'none';
+    wpView.style.display = 'none';
+    jsViewImg.style.display = 'block';
+    phpViewImg.style.display = 'none';
+    wpViewImg.style.display = 'none';
+}
+function showPhpView() {
+    jsView.style.display = "none";
+    phpView.style.display = 'block';
+    wpView.style.display = 'none';
+    jsViewImg.style.display = 'none';
+    phpViewImg.style.display = 'block';
+    wpViewImg.style.display = 'none';
+}
+function showWpView() {
+    jsView.style.display = "none";
+    phpView.style.display = 'none';
+    wpView.style.display = 'block';
+    jsViewImg.style.display = 'none';
+    phpViewImg.style.display = 'none';
+    wpViewImg.style.display = 'block';
+}
+
